37b9d8f38387b0132a14b2cef550340aebbd2f72
chee <cheenicey@hotmail.com>
2022-03-14 21:33:44 +0800

    init chee meter mainnet subgraph

--- src/mappings/ctoken.ts
+++ src/mappings/ctoken.ts
@@ -109,7 +109,7 @@ export function handleRedeem(event: Redeem): void {
   redeem.save()
 }
 
-/* Borrow assets from the protocol. All values either CELO or BEP20
+/* Borrow assets from the protocol. All values either MTR or BEP20
  *
  * event.params.totalBorrows = of the whole market (not used right now)
  * event.params.accountBorrows = total of the account
